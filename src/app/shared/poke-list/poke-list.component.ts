cbb0149d5b07fcc0d2f800fd8420838b8201932b
Daniel Lucas <daniel.lucas1507@gmail.com>
2023-05-12 23:32:23 -0300

    projeto finalizado

---
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
+
+@Component({
+  selector: 'poke-list',
+  templateUrl: './poke-list.component.html',
+  styleUrls: ['./poke-list.component.scss']
+})
+export class PokeListComponent implements OnInit {
+
+  public getAllPokemons : any;
+  private setAllPokemons : any;
+
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
+  constructor(private pokeApi : PokeApiService) { }
+
+  ngOnInit(): void {
+    this.pokeApi.getAll.subscribe(
+      res => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
+      },
+      error =>{
+        this.apiError = true;
+      }
+      );    
+  }
+
+
+  public getSearch(value: string){
+    const filter = this.setAllPokemons.filter( ( res:any ) => {
+      return !res.name.indexOf(value.toLowerCase());
+      
+    });
+    this.isLoading = true;
+    this.getAllPokemons = filter;
+  }
+
+
+}
